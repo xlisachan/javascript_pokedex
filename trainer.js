6fd3573c5e5a97d155ae96d6e509cbf3842c46d6
Lisa Chan <lisachan@NYCDAs-MacBook-Air-3.local>
2018-07-18 15:00:15 -0400

    Trainer Update #1: Trainer Class outline

---
+++ trainer.js
@@ -0,0 +1,17 @@
+class Trainer{
+    constructor(name){
+        this.name = name;
+        // this.hometown = hometown;
+        // this.id = id;
+        this.pokemonCaught = [];
+    }
+
+    all(){
+    }
+
+    get(name){
+    }
+    
+    add(pokemonObject){
+    }
+}
