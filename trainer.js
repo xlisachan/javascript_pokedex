374a842d4add4eb6193a8c89bb4af068d672e2c1
Lisa Chan <lisachan@NYCDAs-MacBook-Air-3.local>
2018-07-18 15:14:49 -0400

    Trainer Update #2: Trainer Class complete

--- trainer.js
+++ trainer.js
@@ -1,3 +1,6 @@
+let trainerList = [];
+let trainers = 0;
+
 class Trainer{
     constructor(name){
         this.name = name;
@@ -7,11 +10,50 @@ class Trainer{
     }
 
     all(){
+        //no parameters
+        //returns an array of Pokemon objects
+        return this.pokemonCaught;
     }
 
     get(name){
+        //accepts one parameter - name
+        //returns a Pokemon object housing info for the pokemon found
+        this.pokemonCaught.filter(element => {
+            if (element.name == name){
+                console.log(element);
+                return element;
+            } else {
+                console.log(`Pokemon not found`);
+                return `Pokemon not found`;
+            }
+        });
     }
     
     add(pokemonObject){
+        axios.get(`https://pokeapi-nycda.firebaseio.com/pokemon/${pokemonObject}.json`).then(response => {
+            let data = response.data;
+            let pokemonName = data.name;
+            let pokemonNumber = data.id;
+            let pokemonType = data.types[0].type.name;
+            let pokeFrontPic = data.sprites.front_default;
+            let pokeBackPic = data.sprites.back_default;
+            let pokemonHp = parseInt(`${data.stats[5].base_stat}`);
+            let pokemonAtk = parseInt(`${data.stats[4].base_stat}`);
+            let pokemonDef = parseInt(`${data.stats[3].base_stat}`);
+            let pokemonAbilities = [];
+            let abilities = data.abilities;
+                abilities.forEach(ability => {
+                    pokemonAbilities.push(ability.ability.name);
+                });
+            
+            let newPokemon = new Pokemon(pokemonName, pokemonNumber, pokemonType, pokeFrontPic, pokeBackPic, pokemonHp, pokemonAtk, pokemonDef, pokemonAbilities);
+
+            // this.pokemonCaught[pokemonName] = newPokemon;
+            this.pokemonCaught.push(newPokemon);
+
+            //evolution
+            // console.log(data);
+        });
     }
+
 }
