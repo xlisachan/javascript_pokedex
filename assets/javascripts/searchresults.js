77f34b280ec6e28e1afd8d7f99d6a6404a29cccf
xlisachan <xlisachan@gmail.com>
2018-11-24 22:19:50 -0500

    Fixed check function

--- assets/javascripts/searchresults.js
+++ assets/javascripts/searchresults.js
@@ -1,39 +1,8 @@
-// Page Elements
-const backButtonDiv = document.createElement('div'),
-      addToPokedex = document.createElement('div');
-      
-backButtonDiv.classList.add('back-button');
-addToPokedex.classList.add('button-div', 'flex-row');
-
-backButtonDiv.innerHTML = '<i class="fas fa-arrow-circle-left"></i>';
-addToPokedex.innerHTML = 'Add To Pokedex';
-
-//Event Listeners
-addToPokedex.addEventListener('click', e => {
-    e.preventDefault();
-    check(newPokemon.id);
-});
-
-backButtonDiv.addEventListener('click', e => {
-    e.preventDefault();
-    login();
-});
-
-// Helper Function
-const check = id => {
-    const found = lisa.pokemonCaught.filter(el => el.id === id);
-    
-    if (found){
-        alert('You already have this pokemon!');
-    } else {
-        lisa.add(id);
-        alert('Pokemon added to pokedex!');
-    }
-}
-
 // Render Function
 const renderSearchResults = newPokemon => {
     const navDiv = document.createElement('div'),
+          backButtonDiv = document.createElement('div'),
+          addToPokedex = document.createElement('div'),
           pokeDiv = document.createElement('div'),
           pokeBasics = document.createElement('div'),
           pokeImage = document.createElement('img'),
@@ -43,8 +12,10 @@ const renderSearchResults = newPokemon => {
           pokeAtk = document.createElement('p'),
           pokeDef = document.createElement('p'),
           pokeAbility = document.createElement('p');
-    
+
     navDiv.classList.add('flex-row', 'jc-sb');
+    backButtonDiv.classList.add('back-button');
+    addToPokedex.classList.add('button-div', 'flex-row');    
     pokeDiv.classList.add('flex-col', 'opaque-div');
     pokeBasics.classList.add('flex-row');
     pokeImage.classList.add('pokemon-img');
@@ -52,6 +23,8 @@ const renderSearchResults = newPokemon => {
     pokeStats.classList.add('poke-stats', 'flex-row');
     pokeAbility.classList.add('poke-abil');  
 
+    backButtonDiv.innerHTML = '<i class="fas fa-arrow-circle-left"></i>';
+    addToPokedex.innerHTML = 'Add To Pokedex';
     pokeImage.src = newPokemon.pic;
     pokePtag.innerHTML = '<span>' + newPokemon.name + '<br/> NO. ' + newPokemon.id + '<br/> TYPE ' + newPokemon.type + '</span>';
     pokeHp.innerHTML = 'HP <br/>' + newPokemon.hp;
@@ -65,4 +38,28 @@ const renderSearchResults = newPokemon => {
     pokeDiv.append(pokeBasics, pokeStats, pokeAbility);
     pokemonSearchResults.append(navDiv, pokeDiv);
     pokemonSearchResults.style.display = 'inline';
+
+    //Event Listeners
+    backButtonDiv.addEventListener('click', e => {
+        e.preventDefault();
+        login();
+    });
+
+    addToPokedex.addEventListener('click', e => {
+        e.preventDefault();
+        check(newPokemon.id);
+        console.log(newPokemon.id)
+    });
+}
+
+// Helper Function
+const check = pokeNumber => {
+    const found = lisa.pokemonCaught.some(el => el.id === pokeNumber);
+    
+    if (found){
+        alert('You already have this pokemon!');
+    } else {
+        lisa.add(pokeNumber);
+        alert('Pokemon added to pokedex!');
+    }
 }
